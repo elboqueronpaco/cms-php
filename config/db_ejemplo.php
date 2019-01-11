74e53392f38a9e73e6f6bacf81592e47d7c8b6f7
elboqueronpaco <elboqueronpaco@gmail.com>
2019-01-11 17:29:33 +0100

    vistas dashoboar, login, registro y variable de entorno

---
+++ config/db_ejemplo.php
@@ -0,0 +1,10 @@
+<?php
+
+    return array(
+        'driver'        => 'mysql',
+        'host'          => 'localhost',
+        'usuarios'      => 'usuario_base_datos',
+        'password'      => 'contraseña_db',
+        'database'      => 'base_de_dato',
+        'charset'       => 'utf8'
+    );
